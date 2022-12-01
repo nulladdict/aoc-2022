3ce0bc12daa9ddd523194719e5d8d5c12839cb99
nulladdict <nulladdicted@gmail.com>
2022-12-01 12:33:02 +0400

    feat(day-01): add solution

--- pkgs/day-01/src/main.rs
+++ pkgs/day-01/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let mut elfs = include_str!("in")
+        .trim()
+        .split("\n\n")
+        .map(|elf| {
+            elf.split("\n")
+                .map(|food| food.parse::<u64>().unwrap())
+                .sum::<u64>()
+        })
+        .collect::<Vec<_>>();
+
+    elfs.sort_by(|a, b| b.cmp(a));
+
+    dbg!(part1(&elfs));
+    dbg!(part2(&elfs));
+}
+
+fn part1(elfs: &[u64]) -> u64 {
+    elfs[0]
+}
+
+fn part2(elfs: &[u64]) -> u64 {
+    elfs.iter().take(3).sum::<u64>()
 }

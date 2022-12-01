7b8a4b41897cb26388da77621f80765125a45080
nulladdict <nulladdicted@gmail.com>
2022-12-01 18:05:55 +0400

    fix(day-01): use nom for parsing

--- pkgs/day-01/src/main.rs
+++ pkgs/day-01/src/main.rs
@@ -1,18 +1,30 @@
+use nom::{
+    character::complete::{digit1, line_ending},
+    combinator::{map, map_res},
+    multi::{count, separated_list1},
+    IResult,
+};
+
 fn main() {
-    let mut elfs = include_str!("in")
-        .trim()
-        .split("\n\n")
-        .map(|elf| {
-            elf.split("\n")
-                .map(|food| food.parse::<u64>().unwrap())
-                .sum::<u64>()
-        })
-        .collect::<Vec<_>>();
+    let mut elfs = parse(include_str!("in")).unwrap().1;
 
     elfs.sort_by(|a, b| b.cmp(a));
 
     dbg!(part1(&elfs));
     dbg!(part2(&elfs));
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<u64>> {
+    separated_list1(
+        count(line_ending, 2),
+        map(
+            separated_list1(
+                line_ending,
+                map_res(digit1, |food: &str| food.parse::<u64>()),
+            ),
+            |elf| elf.into_iter().sum(),
+        ),
+    )(input)
 }
 
 fn part1(elfs: &[u64]) -> u64 {
